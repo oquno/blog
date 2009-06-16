256b4b5b31ae217fe033eb1fa5838101c48c5f7e
oquno <oquno.c@gmail.com>
2009-06-16 10:51:51 +0900

    created blog project

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_blog_session',
+  :secret      => '46b2535ec70c9828367e9345663df923e2661667b73dd3af16c9d98fb269e2030b1c58eae0278d6ee1228acdd665dcad8fa2ddd12216ef0a21bd28b37b75cb6b'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
